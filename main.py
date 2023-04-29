b16d805dbb51f259ff3118e854ff76f6a927b9e9
huyenuet <thanhhuyenk59uet@gmail.com>
2023-04-29 20:54:42 +0700

    feat: add middleware to check user's authencity

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from typing import List
 from fastapi import Depends, FastAPI, HTTPException, Request, Form
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
@@ -6,12 +5,16 @@ from starlette.responses import RedirectResponse
 from sqlalchemy.orm import Session
 import crud, models, schemas
 from database import SessionLocal, engine
+import tokens
+from starlette.status import HTTP_401_UNAUTHORIZED
+import jwt
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+# Dependency to provide a database session object
 def get_db():
     db = SessionLocal()
     try:
@@ -41,17 +44,47 @@ async def login(
     password: str = Form(...),
     next: str = Form()
     ):
-    db_user = crud.get_user_by_email(db, email=username)
-    if not db_user:
-        return JSONResponse({"success": False, "message": "Email is not existed", "redirect_url": None})
-    
-    fake_hashed_password = password + "notreallyhashed"
-    if not (fake_hashed_password == db_user.hashed_password):
-        return JSONResponse({"success": False, "message": "Invalid login credentials", "redirect_url": None})
-    
-    return JSONResponse({"success": True, "message": "Login successful", "redirect_url": next})
+    user = crud.get_user_by_email(db, email=username)
 
+    fake_hashed_password = password + "notreallyhashed"
+    if not user or fake_hashed_password != user.hashed_password:
+        return JSONResponse(content={"success": False, "message": "Incorrect username or password", "redirect_url": None}, 
+                            status_code=HTTP_401_UNAUTHORIZED)
+    
+    # Generate a JWT containing the user ID and an expiration time
+    token = tokens.generate_token(user)
+    
+    response = JSONResponse(content={"success": True, "message": "Login successfully", "redirect_url": next},
+                            status_code=200)
+    # Set the token as a cookie in the response
+    response.set_cookie(key="access_token", value=token)
+
+    return response
 
 @app.get('/home', response_class=HTMLResponse)
 async def get_home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+
+def is_authenticated(request: Request):
+    token = request.cookies.get("access_token")
+    if not token:
+        return False
+    
+    try:
+        payload = tokens.decode_token(token)
+        user_id = int(payload["sub"])
+        print("user_id ", user_id)
+        print("user_id type ", type(user_id))
+        request.state.user_id = user_id
+        return True
+    except (jwt.exceptions.InvalidTokenError, KeyError):
+        return False
+
+
+@app.middleware("http")
+async def middleware(request: Request, call_next):
+    if not is_authenticated(request):
+        if request.url.path not in ["/login", "/docs"]:
+            return RedirectResponse(url="/login")
+    response = await call_next(request)
+    return response
